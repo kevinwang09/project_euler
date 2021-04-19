930e35c1295b6355c2f902f9d360b7b9a7e40dc6
Kevin Wang <kevin.wang@sydney.edu.au>
2021-04-19 23:19:40 +1000

    solved problem 46

---
+++ source.py
@@ -0,0 +1,28 @@
+## Primality test function from wikipedia
+
+def is_prime(n: int) -> bool:
+    """Primality test using 6k+-1 optimization."""
+    if n <= 3:
+        return n > 1
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i ** 2 <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+
+def prime_factors(n):
+    i = 2
+    factors = []
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            n //= i
+            factors.append(i)
+    if n > 1:
+        factors.append(n)
+    return factors
