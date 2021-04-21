2e3d4174590baa34e7d6dfd7af36cb9e25b97d51
Kevin Wang <kevin.wang@sydney.edu.au>
2021-04-21 21:47:32 +1000

    finished problem 44

--- source.py
+++ source.py
@@ -1,5 +1,4 @@
 ## Primality test function from wikipedia
-
 def is_prime(n: int) -> bool:
     """Primality test using 6k+-1 optimization."""
     if n <= 3:
@@ -13,7 +12,7 @@ def is_prime(n: int) -> bool:
         i += 6
     return True
 
-
+## list all prime factors, with repeats
 def prime_factors(n):
     i = 2
     factors = []
@@ -26,3 +25,20 @@ def prime_factors(n):
     if n > 1:
         factors.append(n)
     return factors
+
+
+def triangular(n: int):
+    return(n*(n+1)/2)
+
+def pentagonal(n: int):
+    return(n*(3*n-1)/2)
+
+def hexagonal(n: int):
+    return(n*(2*n-1))
+
+def solve_quad(a: int, b: int, c: int):
+    root1 = (-b+math.sqrt(b**2-4*a*c))/(2*a)
+    if(root1>=0):
+        return(root1)
+    else: 
+        return((-b-math.sqrt(b**2-4*a*c))/(2*a))
